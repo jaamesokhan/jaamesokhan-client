2585d8fb55c323f5beef28892de85902c125ad01
ErfanMojibi <mojibierfan@gmail.com>
2024-06-20 14:48:17 +0330

    add back button to TopBar

--- app/src/main/java/com/example/jaamebaade_client/AppNavHost.kt
+++ app/src/main/java/com/example/jaamebaade_client/AppNavHost.kt
@@ -1,10 +1,8 @@
 package com.example.jaamebaade_client
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -19,7 +17,6 @@ import com.example.jaamebaade_client.ui.theme.JaamebaadeclientTheme
 import com.example.jaamebaade_client.view.DownloadablePoetsScreen
 import com.example.jaamebaade_client.view.DownloadedPoetsScreen
 import com.example.jaamebaade_client.view.PoetCategoryScreen
-import com.example.jaamebaade_client.view.SettingsScreen
 import com.example.jaamebaade_client.view.components.Navbar
 import com.example.jaamebaade_client.view.components.TopBar
 
@@ -33,11 +30,8 @@ fun AppNavHost() {
                     .fillMaxSize()
                     .background(MaterialTheme.colorScheme.background),
                 bottomBar = { Navbar(navController = navController) },
-                topBar = {
-                    TopBar(
-                        innerPadding = PaddingValues(16.dp, 16.dp, 16.dp, 16.dp),
-                    )
-                }
+                topBar = { TopBar(innerPadding = PaddingValues(16.dp, 16.dp, 16.dp, 16.dp), navController = navController) }
+
             ) { innerPadding ->
                 NavHost(navController = navController, startDestination = "downloadedPoetsScreen") {
                     // TODO find a way for referencing the routes NOT as a String
