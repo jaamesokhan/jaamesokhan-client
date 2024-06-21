323c3aaed7fc00317e25b80f43a86abef5715565
Mohammad Khosravi <mohammad.kh@smartech.ir>
2024-06-21 17:46:46 +0330

    fix: add text for not found

--- app/src/main/java/com/example/jaamebaade_client/view/SearchScreen.kt
+++ app/src/main/java/com/example/jaamebaade_client/view/SearchScreen.kt
@@ -3,8 +3,13 @@ package com.example.jaamebaade_client.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -19,26 +24,40 @@ fun SearchScreen(
     navController: NavController,
     searchViewModel: SearchViewModel = hiltViewModel(),
 ) {
+    var hasSearched by remember { mutableStateOf(false) }
+
     val results = searchViewModel.results
     val poets = searchViewModel.allPoets
     Column(modifier = modifier) {
-        SearchBar(
-            modifier = Modifier,
+        SearchBar(modifier = Modifier,
             poets = poets.collectAsState().value,
             onSearchFilterChanged = { searchViewModel.poetFilter = it },
             onSearchQueryChanged = { searchViewModel.query = it }) {
             searchViewModel.search()
+            hasSearched = true
         }
-        SearchResults(results = results, navController = navController)
+        SearchResults(
+            results = results,
+            hasSearched = hasSearched,
+            navController = navController
+        )
 
     }
 }
 
 @Composable
-fun SearchResults(results: List<Verse>, navController: NavController) {
+fun SearchResults(results: List<Verse>, hasSearched: Boolean, navController: NavController) {
     LazyColumn {
-        items(results) { result ->
-            SearchResultItem(modifier = Modifier, result = result, navController = navController)
+        if (results.isEmpty() && hasSearched) {
+            item {
+                Text(text = "جست‌وجوی شما نتیجه‌ای در بر نداشت!")
+            }
+        } else {
+            items(results) { result ->
+                SearchResultItem(
+                    modifier = Modifier, result = result, navController = navController
+                )
+            }
         }
     }
 }
