1f727e30855e030befb00a0d47afbb34c025203b
Erfan Mojibi <79264952+ErfanMojibi@users.noreply.github.com>
2024-06-27 19:35:55 +0330

    add download button to DownloadedPoetsScreen (#16)

--- app/src/main/java/com/example/jaamebaade_client/view/DownloadedPoetsScreen.kt
+++ app/src/main/java/com/example/jaamebaade_client/view/DownloadedPoetsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.jaamebaade_client.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,12 +8,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -45,14 +53,33 @@ fun DownloadedPoetsScreen(
             }
         }
     } else {
-        LazyVerticalGrid(columns = GridCells.Fixed(3), modifier = modifier.padding(8.dp)) {
-            items(poets) { poet ->
-                DownloadedPoet(poet = poet) {
-                    coroutineScope.launch {
-                        val poetCategoryId = downloadedPoetViewModel.getPoetCategoryId(poet.id)
-                        navController.navigate("poetCategoryScreen/${poet.id}/${poetCategoryId}")
+        Box(modifier = modifier.fillMaxSize().padding(8.dp)) {
+            LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+                items(poets) { poet ->
+                    DownloadedPoet(poet = poet) {
+                        coroutineScope.launch {
+                            val poetCategoryId = downloadedPoetViewModel.getPoetCategoryId(poet.id)
+                            navController.navigate("poetCategoryScreen/${poet.id}/${poetCategoryId}")
+                        }
                     }
                 }
+            }
+
+            IconButton(
+                onClick = {
+                    navController.navigate("downloadablePoetsScreen")
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+                    .background(MaterialTheme.colorScheme.primary, shape = CircleShape)
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Poet",
+                    tint = Color.White // TODO hardcoded color?
+                )
             }
         }
     }
