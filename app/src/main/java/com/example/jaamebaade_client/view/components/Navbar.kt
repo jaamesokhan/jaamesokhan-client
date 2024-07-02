c7a0b140967da407fd4a8a163d7def57390d88c0
ErfanMojibi <mojibierfan@gmail.com>
2024-07-02 13:33:08 +0330

    show current menu

--- app/src/main/java/com/example/jaamebaade_client/view/components/Navbar.kt
+++ app/src/main/java/com/example/jaamebaade_client/view/components/Navbar.kt
@@ -3,13 +3,10 @@ package com.example.jaamebaade_client.view.components
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
@@ -17,13 +14,11 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -42,6 +37,7 @@ val routeMap = mapOf(
     "searchScreen" to "searchScreen",
     "favoriteScreen" to "favoriteScreen",
 )
+
 @Composable
 fun Navbar(navController: NavController) {
     val currentRoute = currentRoute(navController, routeMap)
@@ -133,7 +129,6 @@ fun NavbarItem(
         }
     }
 }
-
 
 
 @Composable
