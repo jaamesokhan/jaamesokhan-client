8c8b2baa2b4a7d6f515943f48fb60e2cf03bbbe1
ErfanMojibi <mojibierfan@gmail.com>
2024-07-02 20:40:06 +0330

    - fix font sizes - change navbar size - change search bar size - change phone buttons background to black

--- app/src/main/java/com/example/jaamebaade_client/view/components/Navbar.kt
+++ app/src/main/java/com/example/jaamebaade_client/view/components/Navbar.kt
@@ -49,7 +49,7 @@ fun Navbar(navController: NavController) {
         modifier = Modifier
             .background(color = MaterialTheme.colorScheme.primary)
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(4.dp)
             .navigationBarsPadding(),
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically
