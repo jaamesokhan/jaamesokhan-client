b2e0737e65c8a809bf28fade9ec9cc98a2d0dc1b
ErfanMojibi <mojibierfan@gmail.com>
2024-07-02 14:12:23 +0330

    add system bar padding add download button when no poet downloaded

--- app/src/main/java/com/example/jaamebaade_client/view/components/Navbar.kt
+++ app/src/main/java/com/example/jaamebaade_client/view/components/Navbar.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
