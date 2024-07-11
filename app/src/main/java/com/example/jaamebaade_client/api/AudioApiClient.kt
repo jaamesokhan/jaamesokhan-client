a2db1a5af99e26e95480e3a7ad32f64eae805107
Mohammad Khosravi <mohammad.kh@smartech.ir>
2024-07-11 14:10:52 +0330

    refactor: address merge request issues

--- app/src/main/java/com/example/jaamebaade_client/api/AudioApiClient.kt
+++ app/src/main/java/com/example/jaamebaade_client/api/AudioApiClient.kt
@@ -6,28 +6,28 @@ import javax.inject.Inject
 
 class AudioApiClient @Inject constructor(
     private val audioApiService: AudioApiService,
-){
-    suspend fun getAllRecitations(poemId: Int) : List<AudioData> {
+) {
+    suspend fun getAllRecitations(poemId: Int): List<AudioData> {
         try {
             val res = audioApiService.getAllRecitations(poemId).body()
-            Log.d("AudioApiClient", "getAllRecitations: $res")
             val audioLinks = mutableListOf<AudioData>()
             res?.let {
                 val recitationsMap = it["recitations"] as? List<Map<String, String>>
-                recitationsMap?.forEach {recitation ->
+                recitationsMap?.forEach { recitation ->
                     recitation["mp3Url"].let { mp3Url ->
-                        audioLinks.add(AudioData(
-                            artist = recitation["audioArtist"]!!,
-                            url = mp3Url!!
-                        ))
+                        audioLinks.add(
+                            AudioData(
+                                artist = recitation["audioArtist"]!!,
+                                url = mp3Url!!
+                            )
+                        )
                     }
-
                 }
             }
 
             return audioLinks
         } catch (e: Exception) {
-            Log.e("FUCK", e.message.toString())
+            Log.e("AudioApiClient", "error: ${e.message}")
             return listOf()
         }
     }
