61669a3871d80d59a206f91b3d53b219ace21385
Muhammad Khosravi <49665259+MuhammadKhosravi@users.noreply.github.com>
2024-07-27 15:30:33 +0330

    fix: huge improvements in highlight (#71)

--- app/src/main/java/com/example/jaamebaade_client/view/components/VerseItem.kt
+++ app/src/main/java/com/example/jaamebaade_client/view/components/VerseItem.kt
@@ -8,17 +8,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -37,7 +37,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.jaamebaade_client.model.Highlight
 import com.example.jaamebaade_client.model.Status
@@ -46,6 +45,7 @@ import com.example.jaamebaade_client.utility.toPersianNumber
 import com.example.jaamebaade_client.viewmodel.SelectionOptionViewModel
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VerseItem(
     modifier: Modifier = Modifier,
@@ -65,6 +65,8 @@ fun VerseItem(
 
     var annotatedString by remember { mutableStateOf<AnnotatedString?>(null) }
     val highlightColor = MaterialTheme.colorScheme.tertiary
+    val sheetState = rememberModalBottomSheetState()
+
 
     val context = LocalContext.current
 
@@ -84,11 +86,11 @@ fun VerseItem(
         }
         textFieldValue = TextFieldValue(annotatedString!!)
     }
-
     if (showDialog) {
-        Dialog(onDismissRequest = {
-            showDialog = false
-        }) {
+        ModalBottomSheet(
+            onDismissRequest = { showDialog = false },
+            sheetState = sheetState,
+        ) {
             viewModel.getWordMeaning(
                 word = verse.text.substring(startIndex, endIndex),
                 successCallBack = {
@@ -97,74 +99,68 @@ fun VerseItem(
                 failCallBack = {
                     meaningFetchStatus = Status.FAILED
                 })
-            Surface(
-                shape = RoundedCornerShape(4.dp),
-                modifier = Modifier.defaultMinSize(
-                    minWidth = 300.dp,
-                    minHeight = 200.dp
-                )
-            ) {
-                Column(modifier = Modifier.padding(8.dp)) {
-                    Row(
-                        modifier = Modifier
-                            .padding(4.dp)
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
-                    ) {
-                        Button(onClick = {
-                            highlightCallBack(
-                                startIndex,
-                                endIndex
-                            )
-                            showDialog = false
+            Column(modifier = Modifier.padding(8.dp)) {
+                Row(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
+                ) {
+                    Button(onClick = {
+                        highlightCallBack(
+                            startIndex,
+                            endIndex
+                        )
+                        showDialog = false
 
-                        }) {
-                            Text("هایلایت", style = MaterialTheme.typography.bodySmall)
-                        }
+                    }) {
+                        Text("هایلایت", style = MaterialTheme.typography.bodySmall)
+                    }
 
-                        Button(onClick = {
-                            val clipboard =
-                                context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                            val clip = ClipData.newPlainText(
-                                "جام سخن",
-                                verse.text.substring(startIndex, endIndex)
-                            )
-                            clipboard.setPrimaryClip(clip)
-                            showDialog = false
-                        }) {
-                            Text("کپی", style = MaterialTheme.typography.bodySmall)
+                    Button(onClick = {
+                        val clipboard =
+                            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        val clip = ClipData.newPlainText(
+                            "جام سخن",
+                            verse.text.substring(startIndex, endIndex)
+                        )
+                        clipboard.setPrimaryClip(clip)
+                        showDialog = false
+                    }) {
+                        Text("کپی", style = MaterialTheme.typography.bodySmall)
+                    }
+                }
+                Row(modifier = Modifier.padding(bottom = 8.dp)) {
+                    Text(
+                        text = " معنی:"
+                    )
+                    Text(
+                        text = verse.text.substring(startIndex, endIndex),
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                when (meaningFetchStatus) {
+                    Status.LOADING, Status.NOT_STARTED -> {
+                        Box(modifier = Modifier.height(40.dp)) {
+                            LoadingIndicator()
                         }
                     }
-                    Row {
-                        Text(
-                            text = " معنی:"
-                        )
-                        Text(
-                            text = verse.text.substring(startIndex, endIndex),
-                            fontWeight = FontWeight.Bold
-                        )
+
+                    Status.FAILED -> {
+                        Text("خطا در دریافت معنی")
                     }
-                    when (meaningFetchStatus) {
-                        Status.LOADING, Status.NOT_STARTED -> {
-                            Box(modifier = Modifier.height(40.dp)) {
-                                LoadingIndicator()
-                            }
-                        }
 
-                        Status.FAILED -> {
-                            Text("خطا در دریافت معنی")
-                        }
-
-                        Status.SUCCESS -> {
-                            Text(
-                                text = meaning
+                    Status.SUCCESS -> {
+                        Text(
+                            text = meaning,
+                            modifier = Modifier.padding(
+                                bottom = 10.dp
                             )
-                        }
+                        )
                     }
                 }
             }
         }
-
     }
 
     Row(
@@ -186,6 +182,7 @@ fun VerseItem(
         TextField(
             value = textFieldValue,
             onValueChange = {
+                textFieldValue = it
                 if (it.selection.start != it.selection.end) {
                     startIndex = it.selection.min
                     endIndex = it.selection.max
