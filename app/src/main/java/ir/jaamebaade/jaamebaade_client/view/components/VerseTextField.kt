4baeff0dc76077bd495a9092e4afd1cb2f00063f
Mohammad Khosravi <mohamad138121@gmail.com>
2024-10-04 17:48:56 +0330

    fix: remove extra padding from verse item

--- app/src/main/java/ir/jaamebaade/jaamebaade_client/view/components/VerseTextField.kt
+++ app/src/main/java/ir/jaamebaade/jaamebaade_client/view/components/VerseTextField.kt
@@ -1,18 +1,29 @@
 package ir.jaamebaade.jaamebaade_client.view.components
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalTextToolbar
+import androidx.compose.ui.platform.TextToolbar
+import androidx.compose.ui.platform.TextToolbarStatus
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
@@ -21,6 +32,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VerseTextField(
     textFieldValue: TextFieldValue,
@@ -30,36 +42,70 @@ fun VerseTextField(
     var lastSelection by remember { mutableStateOf(textFieldValue.selection) }
     var job by remember { mutableStateOf<Job?>(null) }
 
-    TextField(
-        value = textFieldValue,
-        onValueChange = {
-            onValueChange(it)
-            if (it.selection != lastSelection) {
-                lastSelection = it.selection
-                job?.cancel()
-                job = CoroutineScope(Dispatchers.Main).launch {
-                    delay(500)
-                    if (lastSelection == it.selection) {
-                        onSelectionChange(it)
+    val interactionSource = remember { MutableInteractionSource() }
+    val textStyle = MaterialTheme.typography.bodyLarge.copy(
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colorScheme.onSurface
+    )
+    val colors = TextFieldDefaults.colors(
+        focusedContainerColor = Color.Transparent,
+        unfocusedContainerColor = Color.Transparent,
+        disabledContainerColor = Color.Transparent,
+        errorContainerColor = Color.Transparent,
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        disabledIndicatorColor = Color.Transparent,
+    )
+    val textColor = textStyle.color
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides colors.textSelectionColors,
+        LocalTextToolbar provides EmptyTextToolbar
+    ) {
+        BasicTextField(
+            value = textFieldValue,
+            onValueChange = {
+                onValueChange(it)
+                if (it.selection != lastSelection) {
+                    lastSelection = it.selection
+                    job?.cancel()
+                    job = CoroutineScope(Dispatchers.Main).launch {
+                        delay(500)
+                        if (lastSelection == it.selection) {
+                            onSelectionChange(it)
+                        }
                     }
                 }
+            },
+            modifier = Modifier
+                .padding(0.dp)
+                .fillMaxWidth(),
+            textStyle = mergedTextStyle,
+            readOnly = true,
+            decorationBox = { innerTextField ->
+                TextFieldDefaults.DecorationBox(
+                    value = textFieldValue.text,
+                    innerTextField = innerTextField,
+                    enabled = true,
+                    singleLine = false,
+                    visualTransformation = VisualTransformation.None,
+                    interactionSource = interactionSource,
+                    contentPadding = PaddingValues(4.dp),
+                    colors = colors
+                )
             }
-        },
-        modifier = Modifier
-            .padding(0.dp)
-            .fillMaxWidth(),
-        textStyle = MaterialTheme.typography.bodyMedium.copy(
-            textAlign = TextAlign.Center
-        ),
-        readOnly = true,
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = Color.Transparent,
-            unfocusedContainerColor = Color.Transparent,
-            disabledContainerColor = Color.Transparent,
-            errorContainerColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent,
-        ),
-    )
+        )
+    }
+}
+
+object EmptyTextToolbar : TextToolbar {
+    override val status: TextToolbarStatus = TextToolbarStatus.Hidden
+    override fun hide() {}
+    override fun showMenu(
+        rect: Rect,
+        onCopyRequested: (() -> Unit)?,
+        onPasteRequested: (() -> Unit)?,
+        onCutRequested: (() -> Unit)?,
+        onSelectAllRequested: (() -> Unit)?,
+    ) = Unit
 }
