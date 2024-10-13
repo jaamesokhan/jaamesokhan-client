9d1bd5ee84dc1b222b200e23347cbb4843748597
Mohammad Khosravi <mohamad138121@gmail.com>
2024-10-13 11:57:30 +0330

    fix: change status handling of downloadable poets

--- app/src/main/java/ir/jaamebaade/jaamebaade_client/view/DownloadablePoetsScreen.kt
+++ app/src/main/java/ir/jaamebaade/jaamebaade_client/view/DownloadablePoetsScreen.kt
@@ -2,7 +2,9 @@ package ir.jaamebaade.jaamebaade_client.view
 
 
 import android.content.Context
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,11 +21,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import ir.jaamebaade.jaamebaade_client.R
 import ir.jaamebaade.jaamebaade_client.model.Poet
+import ir.jaamebaade.jaamebaade_client.model.Status
 import ir.jaamebaade.jaamebaade_client.utility.DownloadStatus
 import ir.jaamebaade.jaamebaade_client.view.components.LoadingIndicator
 import ir.jaamebaade.jaamebaade_client.view.components.PoetItem
@@ -37,7 +43,7 @@ fun DownloadablePoetsScreen(
     poetViewModel: PoetViewModel = hiltViewModel(),
 ) {
     val poets = poetViewModel.poets
-    val isLoading = poetViewModel.isLoading
+    val fetchStatus = poetViewModel.poetFetchStatus
     var searchQuery by remember { mutableStateOf("") }
 
     Column(modifier = modifier) {
@@ -51,23 +57,25 @@ fun DownloadablePoetsScreen(
             modifier = Modifier.fillMaxWidth(),
             textStyle = MaterialTheme.typography.bodyMedium,
         )
-        if (poets.isEmpty() && isLoading) {
+        if (fetchStatus == Status.LOADING) {
             LoadingIndicator()
-        } else if (poets.isEmpty()) {
+        } else if (fetchStatus == Status.FAILED) {
             ServerFailure()
+        } else if (fetchStatus == Status.SUCCESS && poets.isEmpty()) {
+            EmptyPoetResult()
         } else {
             Column {
-                DownloadablePoetsList(poetViewModel, poets, isLoading)
+                DownloadablePoetsList(poetViewModel, poets, fetchStatus)
             }
         }
     }
 }
 
 @Composable
-fun DownloadablePoetsList(
+private fun DownloadablePoetsList(
     poetViewModel: PoetViewModel,
     poets: List<Poet>,
-    isLoading: Boolean,
+    fetchStatus: Status,
 ) {
     val context = LocalContext.current
     val listState = rememberLazyListState()
@@ -86,7 +94,7 @@ fun DownloadablePoetsList(
             }
         }
         item {
-            if (isLoading) {
+            if (fetchStatus == Status.LOADING) {
                 LoadingIndicator()
             }
         }
@@ -105,7 +113,17 @@ fun DownloadablePoetsList(
     }
 }
 
-fun getInternalStorageDir(context: Context): File {
+@Composable
+private fun EmptyPoetResult() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(stringResource(R.string.NO_POET_FOUND))
+    }
+}
+
+private fun getInternalStorageDir(context: Context): File {
     val dir = File(context.filesDir, "poets")
     if (!dir.exists()) {
         dir.mkdirs()
