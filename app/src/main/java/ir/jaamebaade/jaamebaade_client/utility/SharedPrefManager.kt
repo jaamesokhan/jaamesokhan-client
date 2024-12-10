ba989dcb43e1569c381a208d1d681771165452f4
inattentive-nerd <161742515+inattentive-nerd@users.noreply.github.com>
2024-12-10 19:54:37 +0330

    - Fixed a typo in SharedPrefManager - Changed app theme in themes.xml (App won't flash-bang the user at 3 AM anymore, the first scree won't turn white for a second anymore when the app is launching) Be advised that Samsung devices might not handle dynamic colors the way expected: https://github.com/material-components/material-components-android/issues/3924

--- app/src/main/java/ir/jaamebaade/jaamebaade_client/utility/SharedPrefManager.kt
+++ app/src/main/java/ir/jaamebaade/jaamebaade_client/utility/SharedPrefManager.kt
@@ -15,7 +15,7 @@ class SharedPrefManager(
         const val USERNAME_KEY = "Username"
         const val AUTH_TOKEN_KEY = "AuthToken"
         const val APP_THEME_TYPE_KEY = "AppThemeType"
-        const val DYNAMIC_COLOR_Status_KEY = "DynamicColorStatus"
+        const val DYNAMIC_COLOR_STATUS_KEY = "DynamicColorStatus"
         const val SHOW_APP_INTRO_POEM_KEY = "ShowAppIntroPoem"
         const val SHOW_APP_INTRO_MANI_KEY = "ShowAppIntroMain"
         const val SHOW_APP_INTRO_HIGHLIGHT_KEY = "ShowAppIntroHighlight"
@@ -81,13 +81,13 @@ class SharedPrefManager(
 
     fun setColorPreference(color: Boolean) {
         val editor = sharedPreferences.edit()
-        editor.putBoolean(DYNAMIC_COLOR_Status_KEY, color)
+        editor.putBoolean(DYNAMIC_COLOR_STATUS_KEY, color)
         editor.apply()
     }
 
     fun getColorPreference(): Boolean {
         val savedColor =
-            sharedPreferences.getBoolean(DYNAMIC_COLOR_Status_KEY, false)
+            sharedPreferences.getBoolean(DYNAMIC_COLOR_STATUS_KEY, false)
         return savedColor
     }
 
