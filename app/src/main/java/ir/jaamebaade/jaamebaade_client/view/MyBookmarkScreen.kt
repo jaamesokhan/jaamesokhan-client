ec47c7d8038422122f5a1bd90d23381e96c8d419
Erfan <mojibierfan@gmail.com>
2025-03-23 19:22:28 +0330

    feat: add modal sheet

--- app/src/main/java/ir/jaamebaade/jaamebaade_client/view/MyBookmarkScreen.kt
+++ app/src/main/java/ir/jaamebaade/jaamebaade_client/view/MyBookmarkScreen.kt
@@ -1,8 +1,6 @@
 package ir.jaamebaade.jaamebaade_client.view
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,11 +9,20 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -29,10 +36,13 @@ import coil.request.ImageRequest
 import coil.size.Size
 import ir.jaamebaade.jaamebaade_client.R
 import ir.jaamebaade.jaamebaade_client.constants.AppRoutes
+import ir.jaamebaade.jaamebaade_client.model.BookmarkPoemCategoriesPoetFirstVerse
 import ir.jaamebaade.jaamebaade_client.ui.theme.secondaryText
 import ir.jaamebaade.jaamebaade_client.view.components.NewCardItem
 import ir.jaamebaade.jaamebaade_client.viewmodel.MyBookmarkViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyBookmarkScreen(
     modifier: Modifier,
@@ -40,6 +50,10 @@ fun MyBookmarkScreen(
     viewModel: MyBookmarkViewModel = hiltViewModel()
 ){
     val bookmarks = viewModel.bookmarks
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+    var selectedBookmark by remember { mutableStateOf<BookmarkPoemCategoriesPoetFirstVerse?>(null) }
     if(bookmarks.isEmpty()){
 
         Row(modifier = Modifier
@@ -59,35 +73,57 @@ fun MyBookmarkScreen(
             )
         }
     } else {
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-        items(items = bookmarks, key = { it.bookmark.id }) { bookmark ->
-            val painter = rememberAsyncImagePainter(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(bookmark.poet.imageUrl ?: "https://ganjoor.net/image/gdap.png")
-                    .size(Size.ORIGINAL)
-                    .build()
-            )
-            NewCardItem(
-                headerText = bookmark.poem.title,
-                bodyText = AnnotatedString(bookmark.firstVerse.text),
-                image = painter,
-                onClick = { navController.navigate("${AppRoutes.POEM}/${bookmark.poet.id}/${bookmark.poem.id}/-1") },
-                icon = Icons.Filled.MoreVert,
-                onIconClick = {}
-            )
-            HorizontalDivider(
-                modifier = Modifier.padding(
-                    start = 70.dp,
-                    end = 0.dp,
-                    top = 5.dp,
-                    bottom = 5.dp
+        LazyColumn(modifier = modifier.fillMaxSize()) {
+            items(items = bookmarks, key = { it.bookmark.id }) { bookmark ->
+                val painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(bookmark.poet.imageUrl ?: "https://ganjoor.net/image/gdap.png")
+                        .size(Size.ORIGINAL)
+                        .build()
                 )
-            )
+                NewCardItem(
+                    headerText = bookmark.poem.title,
+                    bodyText = AnnotatedString(bookmark.firstVerse.text),
+                    image = painter,
+                    onClick = { navController.navigate("${AppRoutes.POEM}/${bookmark.poet.id}/${bookmark.poem.id}/-1") },
+                    icon = Icons.Filled.MoreVert,
+                    onIconClick = {
+                        selectedBookmark = bookmark
+                        showBottomSheet = true
+                    }
+                )
+                HorizontalDivider(
+                    modifier = Modifier.padding(
+                        start = 70.dp,
+                        end = 0.dp,
+                        top = 5.dp,
+                        bottom = 5.dp
+                    )
+                )
 
+            }
+        }
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState
+            ) {
+                // Sheet content
+                Button(onClick = {
+                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                        if (!sheetState.isVisible) {
+                            showBottomSheet = false
+                        }
+                    }
+                }) {
+                    Text("Hide bottom sheet")
+                }
+            }
         }
     }
-
-        }
-    }
+}
 
 
