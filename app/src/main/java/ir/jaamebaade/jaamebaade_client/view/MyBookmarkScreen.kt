0002bc87331450c74aff4306935202782b8e8eb2
Erfan <mojibierfan@gmail.com>
2025-03-23 17:57:14 +0330

    feat: fiz spacings to match the design

--- app/src/main/java/ir/jaamebaade/jaamebaade_client/view/MyBookmarkScreen.kt
+++ app/src/main/java/ir/jaamebaade/jaamebaade_client/view/MyBookmarkScreen.kt
@@ -1,15 +1,25 @@
 package ir.jaamebaade.jaamebaade_client.view
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -17,7 +27,9 @@ import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Size
+import ir.jaamebaade.jaamebaade_client.R
 import ir.jaamebaade.jaamebaade_client.constants.AppRoutes
+import ir.jaamebaade.jaamebaade_client.ui.theme.secondaryText
 import ir.jaamebaade.jaamebaade_client.view.components.NewCardItem
 import ir.jaamebaade.jaamebaade_client.viewmodel.MyBookmarkViewModel
 
@@ -28,6 +40,25 @@ fun MyBookmarkScreen(
     viewModel: MyBookmarkViewModel = hiltViewModel()
 ){
     val bookmarks = viewModel.bookmarks
+    if(bookmarks.isEmpty()){
+
+        Row(modifier = Modifier
+            .padding(10.dp)
+            .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center) {
+            Icon(
+                imageVector = Icons.Outlined.Info,
+                tint = MaterialTheme.colorScheme.secondaryText,
+                contentDescription = "",
+            )
+            Text(
+                text = stringResource(R.string.NO_BOOKMARK),
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.secondaryText
+            )
+        }
+    } else {
     LazyColumn(modifier = modifier.fillMaxSize()) {
         items(items = bookmarks, key = { it.bookmark.id }) { bookmark ->
             val painter = rememberAsyncImagePainter(
@@ -44,10 +75,17 @@ fun MyBookmarkScreen(
                 icon = Icons.Filled.MoreVert,
                 onIconClick = {}
             )
-            HorizontalDivider(modifier = Modifier.padding(start = 70.dp, end = 0.dp, top = 5.dp, bottom = 5.dp))
+            HorizontalDivider(
+                modifier = Modifier.padding(
+                    start = 70.dp,
+                    end = 0.dp,
+                    top = 5.dp,
+                    bottom = 5.dp
+                )
+            )
 
         }
-
+    }
 
         }
     }
