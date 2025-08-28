511264fb3bd564ba2a4428991c08e207782f3638
Erfan <mojibierfan@gmail.com>
2025-08-28 18:12:50 +0330

    fix: remove unused imports

--- app/src/main/java/ir/jaamebaade/jaamebaade_client/view/components/DownloadablePoetItem.kt
+++ app/src/main/java/ir/jaamebaade/jaamebaade_client/view/components/DownloadablePoetItem.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
